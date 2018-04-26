08b7034dd6b238823e22d2618f4f0781e6f799d0
Nguyen Duc Tuan <ductuan.nguyen@my.jcu.edu.au>
2018-04-26 15:56:17 +0800

    Intital commit.

---
+++ Cosplay Tutorial - The Costuming Resource_files/imagefade.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+				
+				$('.fade2').hide().fadeIn(500);
+				
+			});
---
+++ Cosplay Tutorial - The Costuming Resource_files/in.php
@@ -0,0 +1,3 @@
+// static31
+if( window._genericStats ) _genericStats.set_cookie('_jsuid', '1691030973');var NO_PINGY=1;(function(){var d=new Date;d.setTime(d.getTime()+3600*1000);document.cookie="no_trackyy_66399692=1;expires="+d.toGMTString()+";path=/;";})();
+// exit bad site id
